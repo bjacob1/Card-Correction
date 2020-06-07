a9ee518240fdcbf4768d734ab23e3d448d6debd4
bjacob1 <brian.jacob@outlook.com>
2020-06-07 17:51:51 -0400

    Testing commit

--- main.cpp
+++ main.cpp
@@ -16,6 +16,6 @@ int main()
     for(Point2f &corner : corners)
         circle(img, corner, 5, Scalar(0, 0, 255), -1);
     imshow("Image", blurred);
-    waitKey(0);
+    waitKey(1);
     return 0;
 }
