c6409671df3c569752f1854acc74c883a047e300
bjacob1 <brian.jacob@outlook.com>
2020-06-09 15:11:25 -0400

    gets corners of largest rectangle

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 using namespace cv;
 using namespace std;
 
+int largest_contour(vector<vector<Point>> &contours) {
+    int ind = 0;
+    int size = 0;
+    for(int i = 0; i < contours.size(); i++) {
+        int area = contourArea(contours.at(i));
+        if(area > size) {
+            ind = i;
+            size = area;
+        }
+    }
+    return ind;
+}
+
 int main()
 {
     //read image
@@ -25,7 +38,19 @@ int main()
     //find and draw contours of image
     vector<vector<Point>> contours;
     findContours(canny, contours, noArray(), RETR_CCOMP, CHAIN_APPROX_SIMPLE);
-    drawContours(img, contours, -1, Scalar(0, 0, 255), -1, 8);
+//    drawContours(img, contours, -1, Scalar(0, 0, 255), -1, 8);
+    vector<vector<Point>> test;
+    test.emplace_back(contours.at(0));
+    test.emplace_back(contours.at(1));
+    drawContours(img, test, -1, Scalar(0, 0, 255), -1, 8);
+    RotatedRect rec = minAreaRect(contours.at(largest_contour(contours)));
+    vector<Point2f> corners(4);
+    rec.points(corners.data());
+//    vector<Point> corners;
+//    boxPoints(rec, corners);
+    for(int i = 0; i < corners.size(); i++) {
+        circle(img, corners.at(i), 5, Scalar(0, 0, 255), -1);
+    }
     //display image
     imshow("Image", img);
     waitKey(0);
